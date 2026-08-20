63713593bbf4f0abdd26a747f3cfd71e977a1dc1
simoncock101 <simoncock101@hotmail.com>
2026-08-20 10:27:18 -0500

    Add multi-column layout and interactive elements

--- app.py
+++ app.py
@@ -11,3 +11,30 @@ st.image(image, caption='Interfaces multimodales')
 
 texto = st.text_input('Escribe algo', 'Este es mi texto')
 st.write('El texto escrito es', texto)
+
+st.subheader("Ahora usemos 2 columnas")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+  resp = st.checkbox('Estoy de acuerdo')
+  if resp:
+    st.write('Correcto!')
+
+with col2:
+  st.subheader("Esta es la segunda columna")
+  modo = st.radio("Que modalidad es la principal en tu interfaz", ('Visual', 'auditiva', 'tactil'))
+  if modo == 'Visual':
+    st.write('La vista es fundamental para tu interfaz')
+  if modo == 'auditiva':
+    st.write('La audicion es fundamental para tu interfaz')
+  if modo == 'tactil':
+    st.write('El tatco es fundamental para tu interfaz')
+
+st.subheader("Uso de botones")
+if st.button('Presione el boton'):
+    st.write('Gracias por presionar')
+else:
+  st.write('No has presionado aun')
