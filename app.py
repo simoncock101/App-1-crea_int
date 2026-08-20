569c22ec9653ba31c7ccaa5698cf55d83046cbcc
simoncock101 <simoncock101@hotmail.com>
2026-08-20 10:08:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+from PIL import Image
 
 st.title("HOLA !!! mi  nombre es Simon ")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('Space.jpg')
+st.image(image, caption='Interfaces multimodales')
