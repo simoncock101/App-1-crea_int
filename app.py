2a598ddb405ddab382fbc570170ec9fc3aeb91a6
simoncock101 <simoncock101@hotmail.com>
2026-08-20 10:11:00 -0500

    Add text input and display functionality

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfac
 st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open('Space.jpg')
 st.image(image, caption='Interfaces multimodales')
+
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
